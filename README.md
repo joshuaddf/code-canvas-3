974fbf94f325c707587978402d5d1d7878a0dbcb
joshuaddf <jkdodofoli@gmail.com>
2025-08-31 13:58:23 +0000

    Added banana models and updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-## Trying something new with react three fibre
+# Trying something new with react three fibre
+-This is a new experience. :smile:
+
+## My takes on this.
+- This is really cool. Always nice to learn something new.
+- Requires a lot of power. :rocket;
+- T'was lagging a lot so I had to comment out the EffectComposer.
+
+### To do
+- [x] Built this website.
+- [ ] Built it again on my own to fully understand it.
+- [ ] Add some content.
+- [ ] Add animations using framer-motion.
+
+[^1]: Learnt this from a video on youtube.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,59 @@
 'use client'
 import { Environment, useGLTF } from "@react-three/drei";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { EffectComposer, DepthOfField } from "@react-three/postprocessing";
 import { Suspense, useRef, useState } from "react";
 import * as THREE from "three";
 
-const Box = ({ z }: { z: number }) => {
+const Banana = ({ z }: { z: number }) => {
+  const { nodes, materials } = useGLTF('/banana-transformed.glb');
 
   const { viewport, camera } = useThree();
   const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z]);
-  const boxRef = useRef<THREE.Mesh>(null!);
+  const bananaRef = useRef<THREE.Mesh>(null!);
   const [data] = useState({
     x: THREE.MathUtils.randFloatSpread(width),
     y: THREE.MathUtils.randFloatSpread(height),
+    rX: Math.random() * Math.PI,
+    rY: Math.random() * Math.PI,
+    rZ: Math.random() * Math.PI,
   })
 
   useFrame(() => {
-    boxRef.current.position.set(data.x, data.y += 0.1, z);
+    bananaRef.current.rotation.set((data.rX += 0.003), (data.rY += 0.003), (data.rZ += 0.003));
+    bananaRef.current.position.set(data.x, data.y += 0.01, z);
     if (data.y > height / 1.5) {
       data.y = -height / 1.5;
     }
   })
 
   return (
-    <mesh ref={boxRef}>
-      <boxGeometry />
-      <meshBasicMaterial color="red" />
-    </mesh>
+    <mesh
+      ref={bananaRef}
+      geometry={(nodes.Banana as THREE.Mesh).geometry}
+      material={materials.Banana_High}
+      rotation={[-Math.PI / 2, 0, 0]}
+      material-emissive="orange" />
   )
 }
 
-const Banana = (props: any) => {
-  const { scene } = useGLTF('/banana.glb');
-  return <primitive object={scene} {...props} />;
-}
 
-export default function Home({ count = 600 }: { count: number }) {
+
+export default function Home({ count = 100, depth = 80 }: { count: number, depth: number }) {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <Canvas>
-        {/* { Array.from({ length: count }, (_, i) => {
-        return <Box key={i} z={-i}/>
-      }) } */}
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} intensity={2} />
+      <Canvas gl={{ alpha: false }} camera={{ near: 0.1, far: 110 }}>
+        <color attach="background" args={["#ffbf40"]} />
+        {/* <ambientLight intensity={0.5} /> */}
+        <spotLight position={[10, 10, 10]} intensity={1} />
         <Environment preset="sunset" />
-        <Suspense>
-          <Banana scale={0.5} />
+        <Suspense fallback={null}>
+          {Array.from({ length: count }, (_, i) => {
+            return <Banana key={i} z={-(i / count) * depth} />
+          })}
+        {/* <EffectComposer>
+          <DepthOfField target={[0, 0, depth / 2]} focalLength={0.1} bokehScale={2} height={400} />
+        </EffectComposer> */}
         </Suspense>
       </Canvas>
     </div>
